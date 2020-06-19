f0991628cde0945de43a0e536bf2786a589b0596
Mitul <mitulp236@gmail.com>
2020-06-19 15:54:16 +0530

    good to go

--- surprise/surprise/settings.py
+++ surprise/surprise/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'surprise.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'surprice',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
